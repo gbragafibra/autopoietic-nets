53a8b99425a3a825b21f21704b0e9abaf7034ae4
gbragafibra <gbragafibra@protonmail.com>
2024-10-09 01:32:52 +0100

    run

---
+++ main.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from gates import *
+
+gates = [AND, OR, XOR] #more interesting behaviour without neg gates
+
+#Compute entropy of the net
+def H(S):
+	# S: state matrix
+    counts = np.unique(S, return_counts=True)[1]
+    p = counts/(N**2)
+    return -np.sum(p * np.log(p))
+
+#----------------------------
+"""
+Params and conditions
+"""
+N = 500 #neuron count -> N² neurons generated
+N_iter = 20 #number of iterations
+S = np.random.choice((0,1), size = (N, N)) #init state
+fix = False #to have ε fixed
+ε_fixed = 2 #if ε fixed
+k = 6 #If not fixed -> used denominator -> At max ε -> N_iter/k
+Φ = np.zeros((N, N), dtype=int) #To keep track of synchronization at each neuron/ensemble
+extended = True #Considering also diagonal neighbors
+
+"""
+compare condition between Φ and ε
+If True -> Ensemble when Φ >= ε
+If False -> Ensemble when Φ == ε
+"""
+geq_cond = False 
+#----------------------------
+
+
+# Dynamics for ε
+def dynamics(*args, fixed = False):
+    if fixed: #fixed assignment of ε
+        return ε_fixed
+    else:
+        return int((N_iter * (1 - H(S)))/k)
+
+ε = dynamics(fixed = fix) #init ε
+        
+
+"""
+Init plot
+"""
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+mat1 = ax1.imshow(S, cmap="gray", vmin=0, vmax=1)
+mat2 = ax2.imshow(Φ, cmap="hot", vmin=0, vmax=N_iter)
+ax1.set_title(f"Threshold (ε): {ε}")
+ax1.axis("off")
+ax2.set_title("Φ")
+ax2.axis("off")
+cbar2 = fig.colorbar(mat2, ax=ax2)
+cbar2.set_label("Synchronization Count (Φ)")
+
+
+def update(*args):
+    global S, Φ, ε
+    #choose a gate randomly for each iteration (globally)
+    gate = np.random.choice(gates) 
+
+    #Getting neighbors by shifting matrix
+    left = np.roll(S, -1, axis = 1)
+    right = np.roll(S, 1, axis = 1)
+    up = np.roll(S, 1, axis = 0)
+    down = np.roll(S, -1, axis = 0)
+
+    # stack neighbor states
+    neighbors = np.stack([left, right, up, down], axis = 0)
+
+    #return new state
+    new_state = gate(neighbors)
+    sync = (new_state == S)
+    Φ[sync] += 1 
+    Φ[~sync] = 0
+
+    if geq_cond:
+        mask_ensemble = (Φ >= ε)
+    else:
+        mask_ensemble = (Φ == ε)
+    ε = dynamics(fixed = fix)
+    S = new_state #update state
+    # update given any ensemble formation
+    if np.any(mask_ensemble):
+        ensemble_idxs = np.argwhere(mask_ensemble)
+        for i, j in ensemble_idxs:
+        	#update neighbors given central neuron forming ensemble
+            if extended: #all neighbors(including diagonals)
+                S[(i-1) % N, (j-1) % N] = S[i, j]  #upper left 
+                S[(i-1) % N, (j+1) % N] = S[i, j]  #upper right 
+                S[(i+1) % N, (j+1) % N] = S[i, j]  #bottom right 
+                S[(i+1) % N, (j-1) % N] = S[i, j]  #bottom left
+                S[i, (j-1) % N] = S[i, j]  #left 
+                S[i, (j+1) % N] = S[i, j]  #right 
+                S[(i-1) % N, j] = S[i, j]  #up 
+                S[(i+1) % N, j] = S[i, j]  #down 
+                
+            else: 
+                S[i, (j-1) % N] = S[i, j]  #left 
+                S[i, (j+1) % N] = S[i, j]  #right 
+                S[(i-1) % N, j] = S[i, j]  #up 
+                S[(i+1) % N, j] = S[i, j]  #down 
+    mat1.set_array(S)
+    mat2.set_array(Φ)
+
+    ax1.set_title(f"Threshold (ε): {ε}; Fixed (k: {k if not fix else None}): {fix}; Φ $\geq$ ε: {geq_cond}; Extended: {extended}")
+    return mat1, mat2
+
+ani = FuncAnimation(fig, update, frames=N_iter, interval=1000)
+ani.save("autopoietic_net.gif", writer="pillow", fps=10)
+
+plt.show()
